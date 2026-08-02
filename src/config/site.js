e81303e6b58586a8f799a2df22f5533cfd697e5a
SunFireX3Z <randysaputra1203@gmail.com>
2026-08-02 21:30:03 +0700

    wadaw

--- src/config/site.js
+++ src/config/site.js
@@ -5,5 +5,5 @@ export const siteConfig = {
     url: "https://sunblog.my.id",
     author: "Sun-sama",
     locale: "id_ID",
-    version: "1.9.0",
+    version: "1.10.1",
 };
---
+++ src/content/id/sejarah/alasan-proklamasi-dikumandangkan-di-jakarta/metadata.ts
@@ -0,0 +1,52 @@
+import thumbnailImage from './thumbnail.webp';
+
+export const metadata = {
+    title: "Mengapa Proklamasi Kemerdekaan RI Dikumandangkan di Jakarta? Ini Alasannya",
+    description: "Pelajari alasan mengapa Proklamasi Kemerdekaan Indonesia dibacakan di Jakarta, mulai dari latar belakang sejarah, situasi politik saat itu, hingga pemilihan kediaman Soekarno sebagai lokasi proklamasi.",
+    metaDescription: "Mengapa Proklamasi Kemerdekaan Indonesia dibacakan di Jakarta? Simak sejarah, alasan pemilihan lokasi, serta peran Jakarta dalam peristiwa Proklamasi 17 Agustus 1945.",
+    publishDate: "2 August 2026",
+    lastUpdated: "2 August 2026",
+    updatedReason: "",
+    series: "",
+    status: "Published",
+    author: "Inda Nesya",
+    category: "Sejarah",
+    subcategory: "Kemerdekaan Indonesia",
+    thumbnail: thumbnailImage,
+    imageCaption: "Ilustrasi pembacaan Proklamasi Kemerdekaan Indonesia di Jalan Pegangsaan Timur No. 56, Jakarta.",
+    readingTime: 8,
+    difficulty: "Pemula",
+    platform: [],
+    contentType: "Sejarah",
+    tags: [
+        "Proklamasi Kemerdekaan",
+        "17 Agustus 1945",
+        "Jakarta",
+        "Soekarno",
+        "Mohammad Hatta",
+        "Pegangsaan Timur 56",
+        "Kemerdekaan Indonesia",
+        "Sejarah Indonesia",
+        "Proklamasi Indonesia",
+        "Indonesia"
+    ],
+    featured: false,
+    sources: [
+        {
+            name: "Arsip Nasional Republik Indonesia (ANRI)",
+            url: "https://anri.go.id/"
+        },
+        {
+            name: "Museum Perumusan Naskah Proklamasi",
+            url: "https://kebudayaan.kemdikbud.go.id/muspresnas/"
+        },
+        {
+            name: "Kementerian Pendidikan Dasar dan Menengah Republik Indonesia",
+            url: "https://kemdikbud.go.id/"
+        },
+        {
+            name: "Wikipedia - Proklamasi Kemerdekaan Indonesia",
+            url: "https://id.wikipedia.org/wiki/Proklamasi_Kemerdekaan_Indonesia"
+        }
+    ]
+};
